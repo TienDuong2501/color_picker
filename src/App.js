0717ac64827e4d400bfe774bd168dcbb2aefb417
tienduong2501 <tienduong250196@gmail.com>
2018-04-25 20:03:23 +0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+import './App.css';
+import ColorPicker from './components/ColorPicker';
+import SizeSetting from './components/SizeSetting';
+import Result from './components/Result';
+import Reset from './components/Reset';
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            color : 'red',
+            fontSize : 15
+        };
+        this.onSetColor = this.onSetColor.bind(this);
+        this.onChangeSize = this.onChangeSize.bind(this);
+        this.onSettingDefault = this.onSettingDefault.bind(this);
+    }
+    onSetColor(params) {
+        this.setState({
+            color: params
+        })
+    }
+    onChangeSize(value) {
+            this.setState({
+                fontSize : (this.state.fontSize + value >=8 && this.state.fontSize + value <=36) ? this.state.fontSize + value : this.state.fontSize
+            });
+    }
+    onSettingDefault(value) {
+        if(value) {
+            this.setState({
+                color : 'red',
+                fontSize : 15
+            });
+        };
+    }
+
+  render() {
+    return (
+      <div className="container mt-50">
+        <div className="row">
+         <ColorPicker color={this.state.color} onReceiveColor={ this.onSetColor } />
+         <div className="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+             <SizeSetting fontSize={this.state.fontSize} onChangeSize = { this.onChangeSize } />
+             <Reset onSettingDefault={this.onSettingDefault } />
+        </div>
+         <Result fontSize={this.state.fontSize} color={ this.state.color } />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
